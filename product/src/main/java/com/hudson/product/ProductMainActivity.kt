4b060683e15cf8cfcdf173f92c452379425dbb76
Hudson <2216274479@qq.com>
2022-05-30 14:01:04 +0800

    新增业务组件：product商品组件

---
+++ product/src/main/java/com/hudson/product/ProductMainActivity.kt
@@ -0,0 +1,11 @@
+package com.hudson.product
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_main)
+    }
+}
