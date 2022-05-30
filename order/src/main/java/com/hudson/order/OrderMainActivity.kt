d145737298fd0bbf83e2f5a0d22ba31a6649a11f
Hudson <2216274479@qq.com>
2022-05-30 14:01:44 +0800

    新增业务组件：order订单组件

---
+++ order/src/main/java/com/hudson/order/OrderMainActivity.kt
@@ -0,0 +1,11 @@
+package com.hudson.order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OrderMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_main)
+    }
+}
